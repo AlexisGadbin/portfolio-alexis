4c1a701c3187a84e9bfdde1b4aeef48dca959057
AlexisGadbin <alexis@gadbin.com>
2023-09-18 11:47:08 +0200

    Fix logo sizes

--- components/education/LogoList.jsx
+++ components/education/LogoList.jsx
@@ -4,16 +4,16 @@ function LogoList() {
   return (
     <div className="flex flex-col gap-14">
       <div className="flex gap-10">
-        <Image src="/assets/logos/html.svg" height={100} width={120} />
-        <Image src="/assets/logos/css.svg" height={100} width={120} />
-        <Image src="/assets/logos/javascript.svg" height={100} width={120} />
-        <Image src="/assets/logos/react.svg" height={100} width={120} />
+        <Image src="/assets/logos/html.svg" height={100} width={100} />
+        <Image src="/assets/logos/css.svg" height={100} width={100} />
+        <Image src="/assets/logos/javascript.svg" height={100} width={100} />
+        <Image src="/assets/logos/react.svg" height={100} width={100} />
       </div>
       <div className="flex gap-10">
-        <Image src="/assets/logos/svelte.svg" height={100} width={120} />
-        <Image src="/assets/logos/java.svg" height={100} width={120} />
-        <Image src="/assets/logos/nextjs.svg" height={100} width={120} />
-        <Image src="/assets/logos/spring.svg" height={100} width={120} />
+        <Image src="/assets/logos/svelte.svg" height={100} width={100} />
+        <Image src="/assets/logos/java.svg" height={100} width={100} />
+        <Image src="/assets/logos/nextjs.svg" height={100} width={100} />
+        <Image src="/assets/logos/spring.svg" height={100} width={100} />
       </div>
     </div>
   )
