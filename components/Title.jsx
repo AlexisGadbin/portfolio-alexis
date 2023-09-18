2a2fa0c04c7805c18ad04fd564e3190bf89dec26
Alexis <alexis@gadbin.com>
2023-09-18 18:56:09 +0200

    Fix color in Title

--- components/Title.jsx
+++ components/Title.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-function Title({ title, color = 'white' }) {
+function Title({ title, color = 'text-white' }) {
   return (
     <div className="flex items-center gap-4 pt-16">
-      <h1 className="text-5xl font-bold text-white">{title}</h1>
+      <h1 className={`text-5xl font-bold ${color}`}>{title}</h1>
       <div className="h-[2px] w-[150px] border border-white md:w-[350px]" />
     </div>
   )
