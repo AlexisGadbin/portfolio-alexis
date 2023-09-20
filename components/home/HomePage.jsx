797697b92a9dc83590d9eaa61e1537da122a3553
AlexisGadbin <alexis@gadbin.com>
2023-09-20 16:26:50 +0200

    Responsive home page

--- components/home/HomePage.jsx
+++ components/home/HomePage.jsx
@@ -3,16 +3,16 @@ import Scroller from './Scroller'
 function HomePage() {
   return (
     <section
-      className="relative flex h-[calc(100vh-100px)] items-center justify-center px-[200px]"
+      className="relative flex h-[calc(100vh-100px)] items-center justify-center md:px-[200px]"
       id="home"
     >
-      <div className="pb-16 text-3xl font-semibold">
+      <div className="pb-16 text-base font-semibold md:text-3xl">
         <p className="mb-2">
           Hello, je suis <span className="text-primary">Alexis Gadbin</span>,
         </p>
         <p className="mt-2">Étudiant passioné de développement web</p>
       </div>
-      <div className="absolute bottom-20 right-1/2 -translate-x-1/2">
+      <div className="absolute bottom-20">
         <Scroller />
       </div>
     </section>
