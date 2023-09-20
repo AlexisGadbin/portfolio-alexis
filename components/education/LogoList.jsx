8b1834add54eb45c21a78024e595e2c353f10a5c
AlexisGadbin <alexis@gadbin.com>
2023-09-20 16:43:31 +0200

    Responsive education + fix alt on images

--- components/education/LogoList.jsx
+++ components/education/LogoList.jsx
@@ -2,20 +2,32 @@ import Image from 'next/image'
 
 function LogoList() {
   return (
-    <div className="flex flex-col gap-14">
-      <div className="flex gap-10">
-        <Image src="/assets/logos/html.svg" height={100} width={100} />
-        <Image src="/assets/logos/css.svg" height={100} width={100} />
-        <Image src="/assets/logos/javascript.svg" height={100} width={100} />
-        <Image src="/assets/logos/react.svg" height={100} width={100} />
+    <>
+      <div className="hidden flex-col gap-14 md:flex">
+        <div className="flex gap-10">
+          <Image src="/assets/logos/html.svg" height={100} width={100} alt="HTML" />
+          <Image src="/assets/logos/css.svg" height={100} width={100} alt="CSS" />
+          <Image src="/assets/logos/javascript.svg" height={100} width={100} alt="JavaScript" />
+          <Image src="/assets/logos/react.svg" height={100} width={100} alt="React" />
+        </div>
+        <div className="flex gap-10">
+          <Image src="/assets/logos/svelte.svg" height={100} width={100} alt="Svelte" />
+          <Image src="/assets/logos/java.svg" height={100} width={100} alt="Java" />
+          <Image src="/assets/logos/nextjs.svg" height={100} width={100} alt="NextJS" />
+          <Image src="/assets/logos/spring.svg" height={100} width={100} alt="Spring" />
+        </div>
       </div>
-      <div className="flex gap-10">
-        <Image src="/assets/logos/svelte.svg" height={100} width={100} />
-        <Image src="/assets/logos/java.svg" height={100} width={100} />
-        <Image src="/assets/logos/nextjs.svg" height={100} width={100} />
-        <Image src="/assets/logos/spring.svg" height={100} width={100} />
+      <div className="flex gap-4 overflow-hidden pb-4 md:hidden md:pb-0">
+        <Image src="/assets/logos/html.svg" height={50} width={50} alt="HTML" />
+        <Image src="/assets/logos/css.svg" height={50} width={50} alt="CSS" />
+        <Image src="/assets/logos/javascript.svg" height={50} width={50} alt="JavaScript" />
+        <Image src="/assets/logos/react.svg" height={50} width={50} alt="React" />
+        <Image src="/assets/logos/svelte.svg" height={50} width={50} alt="Svelte" />
+        <Image src="/assets/logos/java.svg" height={50} width={50} alt="Java" />
+        <Image src="/assets/logos/nextjs.svg" height={50} width={50} alt="NextJS" />
+        <Image src="/assets/logos/spring.svg" height={50} width={50} alt="Spring" />
       </div>
-    </div>
+    </>
   )
 }
 
